5fccc37d13e7be4dbaf945582c5134df4760ccc6
Instantki <alvarob.prado@hotmail.com>
2021-06-12 11:35:56 -0400

    create movie repository

--- lib/app/repositories/movie_repository.dart
+++ lib/app/repositories/movie_repository.dart
@@ -1,1 +1,20 @@
-class MovieRepository {}
+import 'package:dio/dio.dart';
+import 'package:teste_tokenlab/app/models/movie_model.dart';
+
+class MovieRepository {
+  final dio = Dio();
+  final baseUrl = 'https://desafio-mobile.nyc3.digitaloceanspaces.com/movies';
+
+  Future fetchMovies() async {
+    try {
+      final response = await dio.get(baseUrl);
+      final responseData = response.data as List;
+      final List<MovieModel> listMovies = responseData.map((movieJson) {
+        return MovieModel.fromJson(movieJson);
+      }).toList();
+      print(listMovies[0]);
+    } catch (error) {
+      print(error);
+    }
+  }
+}
