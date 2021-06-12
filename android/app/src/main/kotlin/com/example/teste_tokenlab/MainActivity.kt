5ccdfefbdfab3cf049e20f25f355f10e8d44a7d8
Instantki <alvarob.prado@hotmail.com>
2021-06-12 10:57:41 -0400

    start project

---
+++ android/app/src/main/kotlin/com/example/teste_tokenlab/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.teste_tokenlab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'app/views/details_view.dart';
+import 'app/views/home_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+      routes: {
+        '/': (context) => HomeView(),
+        '/details': (context) => DetailsView(),
+      },
+    );
+  }
+}
