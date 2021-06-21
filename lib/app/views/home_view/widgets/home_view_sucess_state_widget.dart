2116693d357c37519095e7e7b31aa68aca5346b7
Instantki <alvarob.prado@hotmail.com>
2021-06-21 17:47:18 -0400

    Trying to fixNetworkImage error

--- lib/app/views/home_view/widgets/home_view_sucess_state_widget.dart
+++ lib/app/views/home_view/widgets/home_view_sucess_state_widget.dart
@@ -1,4 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
+// import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:teste_tokenlab/app/controllers/home_controller.dart';
 
