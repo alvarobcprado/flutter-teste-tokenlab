b1a4d4d7d047a322bfa56f9c0c89c68931887ac0
Instantki <alvarob.prado@hotmail.com>
2021-06-21 17:47:00 -0400

    Trying to fixNetworkImage error

--- lib/app/views/home_view/widgets/home_view_sucess_state_widget.dart
+++ lib/app/views/home_view/widgets/home_view_sucess_state_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:teste_tokenlab/app/controllers/home_controller.dart';
 
@@ -34,6 +35,27 @@ class HomeViewSucessStateWidget extends StatelessWidget {
           },
           child: Column(
             children: [
+              Expanded(
+                flex: 10,
+                child: Container(
+                  width: 200,
+                  height: 300,
+                  // child: CachedNetworkImage(
+                  //   imageUrl: controller.moviesList[index].posterUrl,
+                  //   placeholder: (context, _) =>
+                  //       Image.asset('assets/images/placeholder.png'),
+                  //   errorWidget: (context, _, __) => Stack(
+                  //     alignment: Alignment.center,
+                  //     children: [
+                  //       Image.asset(
+                  //         "assets/images/placeholder.png",
+                  //       ),
+                  //       Icon(Icons.warning),
+                  //     ],
+                  //   ),
+                  // ),
+                ),
+              ),
               Expanded(
                 flex: 10,
                 child: FadeInImage(
