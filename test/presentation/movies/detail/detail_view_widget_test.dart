d6a3fbe867b0860941f5798556e4d62b322782bc
Álvaro Prado <56163128+alvarobcprado@users.noreply.github.com>
2023-05-23 21:48:51 -0400

    fix: adjust detail widget test

--- test/presentation/movies/detail/detail_view_widget_test.dart
+++ test/presentation/movies/detail/detail_view_widget_test.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:teste_tokenlab/intl_commons.dart';
 import 'package:teste_tokenlab/presentation/movies/detail/detail_view.dart';
 import 'package:teste_tokenlab/presentation/movies/detail/detail_view_bloc.dart';
@@ -15,8 +16,6 @@ import 'detail_view_widget_test.mocks.dart';
 @GenerateMocks([DetailViewBloc])
 void main() {
   late MockDetailViewBloc detailViewBloc;
-  late PublishSubject<DetailViewState> movieStatePublishController;
-  late PublishSubject<DetailViewAction> eventActionPublishController;
 
   setUpAll(
     () {
@@ -25,15 +24,11 @@ void main() {
   );
 
   setUp(() {
-    movieStatePublishController = PublishSubject<DetailViewState>();
-    eventActionPublishController = PublishSubject<DetailViewAction>();
     detailViewBloc = MockDetailViewBloc();
   });
 
   tearDown(
     () {
-      movieStatePublishController.close();
-      eventActionPublishController.close();
       detailViewBloc.dispose();
     },
   );
@@ -55,17 +50,12 @@ void main() {
   testWidgets(
     'should build movie detail widget when bloc emits success state',
     (tester) async {
-      when(detailViewBloc.stateStream).thenAnswer(
-        (_) => movieStatePublishController,
-      );
-      when(detailViewBloc.actionStream).thenAnswer(
-        (_) => eventActionPublishController,
-      );
-      await _createWidget(tester);
-      await tester.pump();
-      movieStatePublishController.add(
+      when(detailViewBloc.state).thenReturn(
         Success(movieDetail: movieDetailTest),
       );
+
+      await _createWidget(tester);
+      await tester.pump();
       await tester.pumpAndSettle();
       expect(find.byType(DetailViewSucessStateWidget), findsOneWidget);
       expect(
@@ -82,17 +72,10 @@ void main() {
   testWidgets(
     'should build error page when bloc emits error state',
     (tester) async {
-      when(detailViewBloc.stateStream).thenAnswer(
-        (_) => movieStatePublishController,
-      );
-      when(detailViewBloc.actionStream).thenAnswer(
-        (_) => eventActionPublishController,
-      );
+      when(detailViewBloc.state).thenReturn(const Error());
       await _createWidget(tester);
       await tester.pump();
-      movieStatePublishController.add(
-        const Error(),
-      );
+
       await tester.pumpAndSettle();
       expect(find.byKey(const ValueKey('errorPage')), findsOneWidget);
     },
@@ -102,23 +85,22 @@ void main() {
     'should show snackbar with success text when bloc emits '
     ' success state and event action state',
     (tester) async {
-      when(detailViewBloc.stateStream).thenAnswer(
-        (_) => movieStatePublishController,
-      );
-      when(detailViewBloc.actionStream).thenAnswer(
-        (_) => eventActionPublishController,
-      );
-      await _createWidget(tester);
-      await tester.pump();
-      movieStatePublishController.add(
+      final actionCompleter = Completer<void>();
+
+      when(detailViewBloc.state).thenReturn(
         Success(movieDetail: movieDetailTest),
       );
-      await tester.pumpAndSettle();
-      await tester.pump();
+      when(detailViewBloc.listenAction(any)).thenAnswer((invocation) async {
+        await actionCompleter.future;
+        invocation.positionalArguments[0](FavoriteAddedSnackbar());
+      });
 
-      eventActionPublishController.add(
-        FavoriteAddedSnackbar(),
-      );
+      await _createWidget(tester);
+      await tester.pump();
+      await tester.pumpAndSettle();
+      expect(find.byType(DetailViewSucessStateWidget), findsOneWidget);
+
+      actionCompleter.complete();
 
       // fonte: https://github.com/flutter/flutter/blob/master/packages/flutter/test/material/snack_bar_test.dart
       await tester.pump();
@@ -138,27 +120,23 @@ void main() {
     'should show snackbar with error text when bloc emits '
     ' success state and event action error state',
     (tester) async {
-      when(detailViewBloc.stateStream).thenAnswer(
-        (_) => movieStatePublishController,
-      );
-      when(detailViewBloc.actionStream).thenAnswer(
-        (_) => eventActionPublishController,
-      );
+      final actionCompleter = Completer<void>();
+
       when(detailViewBloc.state).thenAnswer(
         (_) => Success(movieDetail: movieDetailTest),
       );
 
+      when(detailViewBloc.listenAction(any)).thenAnswer((invocation) async {
+        await actionCompleter.future;
+        invocation.positionalArguments[0](FavoriteErrorSnackbar());
+      });
+
       await _createWidget(tester);
       await tester.pump();
-      movieStatePublishController.add(
-        Success(movieDetail: movieDetailTest),
-      );
       await tester.pumpAndSettle();
       await tester.pump();
 
-      eventActionPublishController.add(
-        FavoriteErrorSnackbar(),
-      );
+      actionCompleter.complete();
 
       // fonte: https://github.com/flutter/flutter/blob/master/packages/flutter/test/material/snack_bar_test.dart
       await tester.pump();
